cc56d7fee954605be878f4ecc834dd1123df1746
Dennisyk348 <157010776+Dennisyk348@users.noreply.github.com>
2025-01-15 14:29:05 +0800

    Add files via upload
    Updated the Combine move and all the function in combine move is tested

--- src/moves.py
+++ src/moves.py
@@ -1,5 +1,6 @@
 import numpy as np
 from abc import ABC, abstractmethod
+import math
 
 from src.params import Parameters
 from src.params import Tree
@@ -164,30 +165,111 @@ class Split(Move):
         return local_tree
 
     def propose(self, generator):
-        # find node and collect thresholds
-        self.proposed = self.current.copy(self.trees_changed)
-        tree = self.proposed.trees[self.trees_changed[0]]
-        node_id = generator.choice(tree.split_nodes)
-        thresholds,vars = self.collect_values(tree,node_id)
-        # update thresholds and vars
-        tree = self.set_subtree_zero(tree,node_id)
-        tree.vars[node_id] = -1
+        for _ in range(self.tol):
+            # find node and collect thresholds
+            self.proposed = self.current.copy(self.trees_changed)
+            tree = self.proposed.trees[self.trees_changed[0]]
+            node_id = generator.choice(tree.split_nodes)
+            thresholds,vars = Split.collect_values(tree,node_id)
+            # update thresholds and vars
+            tree = Split.set_subtree_zero(tree,node_id)
+            tree.vars[node_id] = -1
         
-        new_thresholds = np.full(len(tree.thresholds), np.nan)
-        new_vars = np.full(len(tree.vars), -2)
-        new_leaf_vals = np.fall(len(tree.leaf_vals),np.nan)
-
-        # give the corresponding value to new tree
-        new_thresholds[:len(thresholds)]=thresholds
-        new_vars[:len(vars)]=vars
-        # generate new tree 
-        tree_new = Tree(data=tree.data, thresholds=new_thresholds, vars=new_vars,
-                        leaf_vals=new_leaf_vals, n=np.fall(len(tree.n),-2),
-                        node_indicators=np.full((tree.data.X.shape[0], len(tree.node_indicators)), 0, dtype=bool))      
-        self.proposed.trees.append(tree_new)
+            new_thresholds = np.full(len(thresholds), np.nan)
+            new_vars = np.full(len(vars), -2)
+            new_leaf_vals = np.fall(len(vars),np.nan)
+            # give the corresponding value to new tree
+            new_thresholds[:len(thresholds)]=thresholds
+            new_vars[:len(vars)]=vars
+            # generate new tree 
+            tree_new = Tree(data=tree.data, thresholds=new_thresholds, vars=new_vars,
+                        leaf_vals=new_leaf_vals, n=np.fall(len(new_thresholds),-2),
+                        node_indicators=np.full((tree.data.X.shape[0], len(new_thresholds)), 0, dtype=bool))  
+            self.proposed.trees.append(tree_new)
+            if tree_new.split_leaf(node_id, vars, thresholds): # If no empty leaves are created
+                return self.proposed
+        self.proposed = self.current
 
         return self.proposed
- 
+
+class Combine(Move):
+    """
+    Move to combine two trees into one tree
+    """
+    # Assume that the input is defined in TreeParams Class
+    def __init__(self, current : Parameters, trees_changed: np.ndarray, tol=100):
+        super().__init__(current, trees_changed)
+        assert len(trees_changed) == 2
+        self.tol = tol
+
+    def find_start_index(tree1,tree2):
+        index = 0
+        for i in tree1.leaves:
+            if tree1.thresholds[i]==tree2.thresholds[0]:
+                index = i
+        if index != 0:
+            return index
+        else:
+            raise ValueError("Can not combine these trees")
+
+    def create_combined(tree1,tree2):
+        d2 = round(math.log2(len(tree2.thresholds)+1))
+        d1 = round(math.log2(len(tree1.thresholds)+1))
+        new_thresholds = np.full(2**(d1+d2)-1, np.nan)
+        new_vars = np.full(2**(d1+d2)-1, -2)
+        new_leaf_vals = np.full(2**(d1+d2)-1,np.nan)
+        new_n = np.full(2**(d1+d2)-1,-2)
+        new_node_indicators = np.full((tree1.data.X.shape[0],2**(d1+d2)-1),0,dtype=bool)
+        # give the corresponding value to new tree
+        new_thresholds[:len(tree1.thresholds)]=tree1.thresholds
+        new_vars[:len(tree1.vars)]=tree1.vars
+        new_leaf_vals[:len(tree1.leaf_vals)] = tree1.leaf_vals
+        new_n[:len(tree1.n)]=tree1.n
+        new_node_indicators=tree1.node_indicators
+        tree_new = Tree(data=tree1.data, thresholds=new_thresholds, vars=new_vars,
+                        leaf_vals=new_leaf_vals, n=new_n,
+                        node_indicators=new_node_indicators)
+        return tree_new  
+
+    
+    def update_tree(tree, start_index, new_vars, new_thresholds):
+        #set the corresponding value of tree A to tree B
+        queue = [start_index]  
+        var_index = 0 
+
+        while queue and var_index < len(new_vars):
+            current_index = queue.pop(0)  
+            if current_index < len(tree.thresholds):  
+            
+                tree.vars[current_index] = new_vars[var_index]
+                tree.thresholds[current_index] = new_thresholds[var_index]
+
+                left_index = 2 * current_index + 1
+                right_index = 2 * current_index + 2
+                queue.append(left_index)
+                queue.append(right_index)
+
+                var_index += 1
+
+        return tree
+
+    def propose(self, generator):
+        for _ in range(self.tol):
+            # find node and collect thresholds
+            self.proposed = self.current.copy(self.trees_changed)
+            tree1 = self.proposed.trees[self.trees_changed[0]]
+            tree2 = self.proposed.trees[self.trees_changed[1]]
+            node_id = Combine.find_start_index(tree1,tree2)
+            combined_tree = Combine.create_combined(tree1,tree2)
+            updated_tree = Combine.update_tree(combined_tree,node_id,tree2.vars,tree2.thresholds)
+            if updated_tree.update_n(node_id): 
+                self.proposed.trees.remove(tree1)
+                self.proposed.trees.remove(tree2)
+                self.proposed.trees.append(updated_tree)
+                return self.proposed
+        self.proposed = self.current
+        return self.proposed
+
 all_moves = {"grow" : Grow,
             "prune" : Prune,
             "change" : Change,
