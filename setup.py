f92056a63ef59f2c99b1f00f9de721f1ead7952a
drizmiz <drizmiz@outlook.com>
2025-11-10 03:29:03 +0800

    Update package management files

--- setup.py
+++ setup.py
@@ -1,41 +1,9 @@
-from setuptools import setup, find_packages
+"""
+Minimal setup.py for backward compatibility.
 
-runtime_deps = [
-    "numpy",
-    "scipy",
-    "pandas",
-    "scikit-learn",
-    "numba",
-    "arviz",
-    "pydantic",
-    "tqdm",
-    "graphviz",
-    "ray[default]",
-]
+All configuration is now in pyproject.toml.
+This file is kept for compatibility with older versions of pip and setuptools.
+"""
+from setuptools import setup
 
-test_extras = [
-    "pytest",
-    "xgboost",
-    "bartz",
-    "pmlb",
-]
-
-setup(
-    name="bart_playground",
-    version="0.1.1",
-    packages=find_packages(),
-    install_requires=runtime_deps,
-    extras_require={
-        "test": test_extras,
-    },
-    python_requires=">=3.7",
-    description="A fast and modular implementation of BART",
-    author="Yan Shuo Tan",
-    author_email="yanshuo@nus.edu.sg",
-    url="https://github.com/yanshuotan/bart-playground",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
